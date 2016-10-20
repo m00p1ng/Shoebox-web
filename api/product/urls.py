319f0cc444c0a6769983eae807eee878e5d55cdb
moopings <mongkonchai4412@gmail.com>
2016-10-20 11:28:52 +0700

    add some app

--- api/product/urls.py
+++ api/product/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^all/$', views.product_all),
     url(r'^name/(?P<name>(.+))/$', views.product_name),
+    url(r'^size/(?P<size>(.+))/$', views.product_size),
 ]
--- api/product/views.py
+++ api/product/views.py
@@ -8,6 +8,9 @@ def product_all(request):
     return HttpResponse(response_data, content_type="application/json")
 
 def product_name(request, name):
-    print(name)
     response_data = Product.objects(prodname=name).to_json()
+    return HttpResponse(response_data, content_type="application/json")
+
+def product_size(request, size):
+    response_data = Product.objects(size=size).to_json()
     return HttpResponse(response_data, content_type="application/json")
