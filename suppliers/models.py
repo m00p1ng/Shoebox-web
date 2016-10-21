8d0d50b7225d31540cb0477322540d736a9dfd14
stsupertam <supmantam@gmail.com>
2016-10-22 01:53:34 +0700

    Add model attribute

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from mongoengine import *
 # Create your models here.
+
+class Supplier(Document):
+    supName = StringField(max_length=50, required=True)
+    supContactFName = StringField(max_length=50, required=True)
+    supContactLName = StringField(max_length=10, required=True)
+    contactTitle = StringField(max_length=150, required=True)
+    supCity = StringField(max_length=50, required=True)
+    supDistrict = StringField(max_length=50, required=True)
+    supStreet = StringField(max_length=50, required=True)
+    supZip = StringField(max_length=10, required=True)
+    supContactPhone = StringField(max_length=10, required=True)
+    supEmail = StringField(max_length=50, required=True)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from mongoengine import *
 
 # Create your models here.
+
+class Users(EmbeddedDocument):
+    userId = ObjectIdField()
+    userRole = StringField(max_length=10, required=True) 
+    username = StringField(max_length=50, unique=True, required=True)
+    password = StringField(max_length=50, required=True)
+    email = StringField(max_length=50, required=True)
+
+class Employees(Document):
+    empFName = StringField(max_length=50, required=True)
+    empLName = StringField(max_length=50, required=True)
+    gender = StringField(max_length=10, required=True)
+    dateOfB = DateTimeField(required=True)
+    empCity = StringField(max_length=50, required=True)
+    empDistrict = StringField(max_length=50, required=True)
+    empStreet = StringField(max_length=50, required=True)
+    empZip = StringField(max_length=10, required=True)
+    empPhone = StringField(max_length=10, required=True)
+    dateEntered = DateTimeField(required=True)
+    users = ListField(EmbeddedDocumentField(Users))
+
+class Customers(Document):
+    custFName = StringField(max_length=50, required=True)
+    custLName = StringField(max_length=50, required=True)
+    gender = StringField(max_length=10, required=True)
+    dateOfB = DateTimeField(required=True)
+    custCity = StringField(max_length=50, required=True)
+    custDistrict = StringField(max_length=50, required=True)
+    custStreet = StringField(max_length=50, required=True)
+    custZip = StringField(max_length=10, required=True)
+    custPhone = StringField(max_length=10, required=True)
+    creditType = StringField(max_length=10, required=True)
+    creditId = StringField(max_length=16, required=True)
+    creditEXP = DateTimeField(required=True)
+    shipCity = StringField(max_length=50, required=True)
+    shipDistrict = StringField(max_length=50, required=True)
+    shipStreet = StringField(max_length=50, required=True)
+    shipZip = StringField(max_length=10, required=True)
+    dateEntered = DateTimeField(required=True)
+    users = ListField(EmbeddedDocumentField(Users))
