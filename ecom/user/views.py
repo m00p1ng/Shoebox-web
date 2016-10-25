8d57de8d6cd14b54d125e064d05481b0dd6d9342
moopings <mongkonchai4412@gmail.com>
2016-10-25 20:17:25 +0700

    add user login

--- ecom/user/views.py
+++ ecom/user/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from django.contrib.auth import login, logout
-from .models import *
+from mongoengine.django.auth import User
+import json
 
-# Create your views here.
-
+@csrf_exempt
 def login(request):
     if request.method == 'POST':
-        pass
-
+        data = json.loads(request.body.decode())
+        username = data['username']
+        password = data['password']
+        user = User.objects(username=username).first()
+        if user and user.check_password(password):
+            request.session['username'] = username
+            request.session['role'] = user.role
+            return HttpResponse(username)
+        else:
+            return HttpResponse('Username or password not correct')
 
 def logout(request):
-    pass
+    request.session.flush()
+    return HttpResponse("logout")
 
-def register(request):
-    pass
+def test_view(request):
+    if 'username' in request.session:
+        return HttpResponse(request.session['username'])
+    else:
+        return HttpResponse('Not login')
