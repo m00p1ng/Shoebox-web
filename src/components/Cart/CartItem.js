f9144c0a81b995eb09118104b7709bdbc9c16fdf
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-01 23:36:23 +0700

    added Cart Item

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 const CartItem = ({product}) => (
-  <h1>{product}</h1>
+  <div className="card horizontal">
+						<div className="row">
+							<div className="col s12 l4">
+								<div>
+									<img className="responsive-img" src="{product.picture}" alt="cart_img" />
+								</div>
+							</div>
+
+							<a href="#"><i className="material-icons cart_bin">cancel</i></a>
+
+							<div className="col s12 l6">
+								<div className="card-content cart-content_detail">
+									<p className="brand_in_cart">{product.brand}</p>
+									<p className="name_in_cart">{product.name}</p>
+									<div className="divider"></div>
+									<p className="price_in_cart">Price: ${product.price}</p>
+									<p className="quantity_in_cart">Quantity: 1</p>
+								</div>
+							</div>
+						</div>
+					</div>
 )
 
 export default CartItem
