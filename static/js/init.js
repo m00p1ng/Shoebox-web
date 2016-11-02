5ae3e6c8391e27626c723dc6cf7db9a34c229b44
moopings <mongkonchai4412@gmail.com>
2016-11-02 14:57:54 +0700

    add ignore file

--- static/js/init.js
+++ static/js/init.js
@@ -1,14 +1,14 @@
 (function($){
   $(function(){
-      
+
       $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
       });
-      
+
       $(".dropdown-button").dropdown();
-      
+
       $('.carousel.carousel-slider').carousel({full_width: true,
                          time_constant: 200});
       window.setInterval(function(){$('.carousel').carousel('next')},5000)
