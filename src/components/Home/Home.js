2a4f58f7faaebe7559745d9dd44b47883ad95ad6
moopings <mongkonchai4412@gmail.com>
2016-11-07 13:03:29 +0700

    carousel fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Carousel from './Carousel'
 import { ProductListApp } from '../../containers'
+import '../../../static/js/jquery-2.1.1.min.js'
 
-const Home = () => (
-  <div>
-    <Carousel />
-    <ProductListApp title="New Arrival"/>
-  </div>
-)
+class Home extends Component {
+  componentDidMount() {
+    $('.carousel.carousel-slider').carousel({full_width: true,
+                       time_constant: 200});
+    window.setInterval(function(){$('.carousel').carousel('next')},5000)
+  }
+
+  render() {
+    return (
+      <div>
+        <Carousel />
+        <ProductListApp title="New Arrival"/>
+      </div>
+    )
+  }
+}
 
 export default Home
