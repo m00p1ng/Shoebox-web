c0b8daf988dddb9875f22f3c54ef274f80264e0d
moopings <mongkonchai4412@gmail.com>
2016-11-08 22:53:34 +0700

    remove unused packages

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import products from './product'
 import cart from './cart'
 import user from './user'
 
 const rootReducer = combineReducers({
+  form: formReducer,
   products,
   cart,
   user
