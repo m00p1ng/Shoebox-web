ba049a5bc8ca16925ad724c69844725e4ca96f2d
moopings <mongkonchai4412@gmail.com>
2016-11-09 08:22:35 +0700

    improved customer script

--- api/script/customer.py
+++ api/script/customer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 from faker import Faker
+from pprint import pprint
 import requests
 import time
 import sys
 import json
 
 fake = Faker()
+
+
+def response_print(res):
+    print()
+    print("Response:")
+    print(res.text)
+    print('--------------------')
+    print()
 
 
 def gen_customer(total):
@@ -19,78 +28,85 @@ def gen_customer(total):
             "email": fake.email(),
             "firstname": fake.first_name(),
             "lastname":  fake.last_name(),
+            "phone": fake.phone_number(),
             "picture" : "picture url",
-            "gender": "male",
+            "gender": fake.random_element(elements=('male', 'female')),
             "birthday": {
-                "year": 2000,
-                "month": 10,
-                "day": 10
-                },
+                "year": int(fake.year()),
+                "month": int(fake.month()),
+                "day": int(fake.day_of_month())
+            },
             "address": {
                 "city": fake.city(),
-                "district": "eiei",
+                "district": fake.state(),
                 "street": fake.street_name(),
                 "zipcode": fake.zipcode()
-                },
+            },
             "ship": {
                 "city": fake.city(),
-                "district": "my-district",
+                "district": fake.state(),
                 "street": fake.street_name(),
                 "zipcode": fake.zipcode()
-                },
-            "phone": fake.phone_number(),
+            },
             "credit" : {
-                "type" : "XXX",
-                "id" : fake.credit_card_number,
+                "type" : fake.random_element(elements=('visa', 'master')),
+                "id" : fake.credit_card_number(),
                 "exp" : {
                     "year": 2000,
                     "month": 10
                 }
-            },
+            }
         }
 
         res = requests.post(URL, data=json.dumps(payload))
 
-        if res.status_code == 200:
+        if res.status_code == 200 or res.status_code == 201:
             print(username, "was created")
         else:
             print(username, "wasn't created")
 
-        print(res.text)
-        print()
+        response_print(res)
 
 
 def delete_customer(username):
     URL = "http://localhost:8000/api/user/customer/%s" % username
     res = requests.delete(URL)
 
-    if res.status_code == 200:
+    if res.status_code == 200 or res.status_code == 201:
         print(username, "was deleted")
     else:
         print(username, "wasn't deleted")
 
-    print(res.text)
-    print()
+    response_print(res)
 
 
-def find_customer(username):
-    URL = "http://localhost:8000/api/user/customer/%s" % username
+def find_customer(username=0):
+    URL = ""
+    if username == 0:
+        URL = "http://localhost:8000/api/user/customer"
+    else:
+        URL = "http://localhost:8000/api/user/customer/%s" % username
 
     res = requests.get(URL)
 
-    if res.status_code == 200:
+    if res.status_code == 200 or res.status_code == 201:
         print(username, "found")
     else:
         print(username, "not found")
 
-    print(res.text)
-    print()
+    response_print(res)
 
 
 if __name__ == '__main__':
     if sys.argv[1] == 'create':
-        gen_customer(int(sys.argv[2]))
+        if len(sys.argv) == 3:
+            gen_customer(int(sys.argv[2]))
+        else:
+            gen_customer(1)
     elif sys.argv[1] == 'delete':
         delete_customer(sys.argv[2])
     elif sys.argv[1] == 'find':
-        find_customer(sys.argv[2])
+        if len(sys.argv) == 3:
+            find_customer(sys.argv[2])
+        else:
+            find_customer()
