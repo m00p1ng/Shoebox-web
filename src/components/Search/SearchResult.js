94502740f6f26f0a5e55fe522a0d30eb8bd969bb
moopings <mongkonchai4412@gmail.com>
2016-11-11 09:22:16 +0700

    refactor product detail and search

--- src/components/Search/SearchResult.js
+++ src/components/Search/SearchResult.js
@@ -1,39 +1,32 @@
 import React from 'react'
 import SearchItem from './SearchItem'
 
+const render_list = (list) => (
+  list.map(item => {
+    return (
+      <SearchItem
+        key={item.slug}
+        product={item}/>
+    )
+  })
+)
+
+const renderResult = (result, hasError, isLoading ,searchText) => {
+  // if(isLoading) return(<h1>Loading...</h1>)
+  if (hasError) return (<h3>Oh sorry, <strong>{searchText}</strong> Not found</h3>)
+  if (result.length > 0) return render_list(result)
+  return (<h3>What are you looking for..</h3>)
+}
 
 const SearchResult = ({
   result,
   hasError,
   searchText,
   isLoading
-}) => {
-  const render_list = (list) => (
-    list.map(item => {
-      return (
-        <SearchItem
-          key={item.slug}
-          product={item}/>
-      )
-    })
-  )
-
-  const renderResult = (result, hasError, isLoading) => {
-    // if(!isLoading) {
-      if (!hasError) {
-        if (result.length > 0) return render_list(result)
-        else return (<h3>What are you looking for..</h3>)
-      }
-      else return (<h3>Oh sorry, {searchText} Not found</h3>)
-    // }
-    // else return(<h1>Loading...</h1>)
-  }
-
-  return (
-    <div>
-      {renderResult(result, hasError, isLoading)}
-    </div>
-  )
-}
+}) => (
+  <div>
+    {renderResult(result, hasError, isLoading, searchText)}
+  </div>
+)
 
 export default SearchResult
