8966d9613347183e1a5efb7b5ee76a03a624f8b5
DADDYofALL <thurisaz.st@gmail.com>
2016-11-14 13:08:24 +0700

    ไม่บอกหรอก อิอิ

--- api/user/tests/employee_api/create_api.py
+++ api/user/tests/employee_api/create_api.py
@@ -43,7 +43,7 @@ class employee_Create_Fail_API_Test(MongoTestCase):
     URL = '/api/user/employee'
     URL_USERNAME = '/api/user/employee/mooping12345'
 
-    def test_create_no_firstname(self):
+    def test_create_no_data(self):
         CREATE_BODY = """{}"""
         res = create_request(self.URL, CREATE_BODY)
         data = json.loads(res.content.decode())
@@ -68,44 +68,13 @@ class employee_Create_Fail_API_Test(MongoTestCase):
         ])
         self.assertEqual(data['created'], False)
 
-    def test_create_no_data(self):
+    def test_create_JSON(self):
         CREATE_BODY = ""
 
         res = create_request(self.URL, CREATE_BODY)
         data = json.loads(res.content.decode())
 
         self.assertEqual(data['errorMsg'], ['JSON Decode error'])
-        self.assertEqual(data['created'], False)
-
-    def test_create_username_duplicated(self):
-        CREATE_BODY = """
-       {
-        	"username": "mooping12345",
-        	"password": "secret",
-        	"repassword": "secret",
-        	"email": "mail@gmail.com",
-        	"firstname": "kaoneaw",
-        	"lastname": "mooping",
-            "picture": "picture URL",
-        	"gender": "male",
-        	"birthday": {
-        		"year": 2000,
-        		"month": 10,
-       		"day": 10
-        	},
-        	"address": {
-       		"city": "my-city",
-        		"district": "my-district",
-        		"street": "my-street",
-       		"zipcode": "99999"
-        	},
-        	"phone": "080-000-0000"
-        }
-        """
-
-        res = create_request(self.URL, CREATE_BODY)
-        data = json.loads(res.content.decode())
-        self.assertEqual(data['errorMsg'], ['Username already exist'])
         self.assertEqual(data['created'], False)
 
     def test_create_username_duplicated(self):
