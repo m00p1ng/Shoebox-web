6c74973a628f4e5f1c3409453d4c4023f885d878
moopings <mongkonchai4412@gmail.com>
2016-11-16 20:36:09 +0700

    delete order detail api

--- api/order/orderDetail/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.orderDetail),
-    url(r'^(?P<orderNumber>.+?)/?$', views.orderDetail_with_orderNumber)
-
-]
