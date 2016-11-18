1e20865936e36aca613827a28203a05ff8863a7f
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-18 14:22:49 +0700

    added ManageMenu

--- src/components/Manage/ManageApp.js
+++ src/components/Manage/ManageApp.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { HeaderBarApp } from './HeaderBarApp'
+import ManageMenu from './ManageMenu'
 
 const ManageApp = () => (
   <div>
     <HeaderBarApp />
+
+    <div className="container" >
+      <ManageMenu />
+    </div>
   </div>
 )
 
