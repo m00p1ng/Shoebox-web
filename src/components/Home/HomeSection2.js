0752a8af338f93f3e31e8a267d6c298fe19f9457
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-21 14:41:06 +0700

    change some style

--- src/components/Home/HomeSection2.js
+++ src/components/Home/HomeSection2.js
@@ -5,8 +5,8 @@ import { URL_ROOT } from 'endpoint'
 const HomeSection2 = () => (
   <div className="section white" >
     <div className="row container center" >
-      <h4>You can't buy happiness, but you can buy shoes.</h4>
-      <h4>And that's kind of the same thing.</h4>
+      <h4 className="sb-bold">You can't buy happiness, but you can buy shoes.</h4>
+      <h4 className="sb-bold">And that's kind of the same thing.</h4>
       <div className="sb-home-shop-now-btn" >
         <Link
           to={`${URL_ROOT}/shop`}
