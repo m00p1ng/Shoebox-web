796ac760c8a2fef4f879654be68d23aa6efe8b38
moopings <mongkonchai4412@gmail.com>
2016-11-21 12:54:58 +0700

    reformat user login

--- api/user/views.py
+++ api/user/views.py
@@ -46,11 +46,11 @@ def login(request):
     if request.method == 'POST':
         try:
             if 'username' in request.session:
-                res = {
+                res = json.dumps({
                     "username": request.session['username'],
                     "role": request.session['role']
-                }
-                return HttpResponse(json.dumps(res), content_type=json_type)
+                })
+                return HttpResponse(res, content_type=json_type)
 
             data = json.loads(request.body.decode())
             username = data['username']
@@ -60,29 +60,41 @@ def login(request):
                 request.session['username'] = username
                 request.session['role'] = user.role
                 request.session.save()
-                res = {
+                res = json.dumps({
                     "username": request.session['username'],
                     "role": request.session['role']
-                }
-                return HttpResponse(json.dumps(res), content_type=json_type)
+                })
+                return HttpResponse(res, content_type=json_type)
             elif username == '' or password == '':
                 if username == '' and password == '':
-                    errmsg = '{"errorMsg": "Username and password cannot empty"}'
+                    errmsg = json.dumps({
+                        "errorMsg": "Username and password cannot empty"
+                    })
                 elif username == '':
-                    errmsg = '{"errorMsg": "Username cannot empty"}'
+                    errmsg = json.dumps({
+                        "errorMsg": "Username cannot empty"
+                    })
                 elif password == '':
-                    errmsg = '{"errorMsg": "Password cannot empty"}'
+                    errmsg = json.dumps({
+                        "errorMsg": "Password cannot empty"
+                    })
                 return HttpResponse(errmsg, content_type=json_type, status=401)
             else:
-                errmsg = '{"errorMsg": "Username or password incorrect"}'
+                errmsg = json.dumps({
+                    "errorMsg": "Username or password incorrect"
+                })
             return HttpResponse(errmsg, content_type=json_type, status=401)
 
         except ValueError as e:
-            errmsg = '{"errorMsg": "JSON Decode error"}'
-            return HttpResponse(errmsg, status=400)
+            errmsg = json.dumps({
+                "errorMsg": "JSON Decode error"
+            })
+            return HttpResponse(errmsg, content_type=json_type, status=400)
         except KeyError as e:
-            errmsg = '{"errorMsg": "Username or password cannot empty"}'
-            return HttpResponse(errmsg, status=400)
+            errmsg = json.dumps({
+                "errorMsg": "Username or password cannot empty"
+            })
+            return HttpResponse(errmsg, content_type=json_type, status=400)
 
     if request.method == 'GET':
         return HttpResponse('Method not allowed', status=405)
@@ -92,7 +104,7 @@ def login(request):
 def logout(request):
     if 'username' in request.session:
         request.session.flush()
-        logged_out = {"logged_out": True}
+        logged_out = json.dumps({"logged_out": True})
     else:
-        logged_out = {"logged_out": False}
-    return HttpResponse(json.dumps(logged_out), content_type=json_type)
+        logged_out = json.dumps({"logged_out": False})
+    return HttpResponse(logged_out, content_type=json_type)
