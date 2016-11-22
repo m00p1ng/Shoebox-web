bc21664ab7b21a70a545fea46d4a8863d74f483d
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-22 18:44:01 +0700

    shop new style

--- src/components/Shop/ShopList.js
+++ src/components/Shop/ShopList.js
@@ -44,23 +44,38 @@ const ShopPagination = ({row, handlePage, activePage}) => (
 )
 
 const ShopItem = ({product}) => (
-  <div className="col l3 m4 s6">
-    <div className="sb-shop-item center">
-  		<div className="sb-hover-zoom">
-  			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
-  				<img
-            src={product.picture}
-            alt={product.slug} />
-  			</Link>
-  			<span className="chip">{product.brand}</span>
-  			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
-          {product.name}
-        </Link>
-  			<div className="sb-shop-item-price">
-          Price: $ {product.price}
-        </div>
-  		</div>
-    </div>
+  // <div className="col l3 m4 s6">
+  //   <div className="sb-shop-item center">
+  // 		<div className="sb-hover-zoom">
+  // 			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
+  // 				<img
+  //           src={product.picture}
+  //           alt={product.slug} />
+  // 			</Link>
+  // 			<span className="chip">{product.brand}</span>
+  // 			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
+  //         {product.name}
+  //       </Link>
+  // 			<div className="sb-shop-item-price">
+  //         Price: $ {product.price}
+  //       </div>
+  // 		</div>
+  //   </div>
+	// </div>
+
+	<div className="col l3 m4 s6 sb-shop-item">
+			<div>
+				<img src={product.picture} alt={product.slug} />
+			</div>
+			<div className="sb-shop-text-section" >
+				<span className="sb-bold black-text sb-shop-small-text">{product.brand}</span>
+				<br />
+				<span className="sb-shop-small-text">{product.name}</span>
+				<br />
+				<div className="sb-shop-price-box">
+					<span className="sb-shop-small-text sb-shop-price-text">$ {product.price}</span>
+				</div>
+			</div>
 	</div>
 )
 
@@ -79,7 +94,7 @@ const ShopList = ({
 	activePage
 }) => (
 	<div className="col l10 s12">
-		<div className="card grey lighten-4">
+		<div className="card white">
       <div className="row">
         {children}
       </div>
