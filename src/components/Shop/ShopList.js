aba294a66e1c64d4bf5ec6c117854f52ec7f36fa
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-22 20:22:02 +0700

    finish shop page v2

--- src/components/Shop/ShopList.js
+++ src/components/Shop/ShopList.js
@@ -44,36 +44,23 @@ const ShopPagination = ({row, handlePage, activePage}) => (
 )
 
 const ShopItem = ({product}) => (
-  // <div className="col l3 m4 s6">
-  //   <div className="sb-shop-item center">
-  // 		<div className="sb-hover-zoom">
-  // 			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
-  // 				<img
-  //           src={product.picture}
-  //           alt={product.slug} />
-  // 			</Link>
-  // 			<span className="chip">{product.brand}</span>
-  // 			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
-  //         {product.name}
-  //       </Link>
-  // 			<div className="sb-shop-item-price">
-  //         Price: $ {product.price}
-  //       </div>
-  // 		</div>
-  //   </div>
-	// </div>
-
 	<div className="col l3 m4 s6 sb-shop-item">
 			<div>
-				<img src={product.picture} alt={product.slug} />
+			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
+				<img
+					src={product.picture}
+					alt={product.slug} />
+			</Link>
 			</div>
 			<div className="sb-shop-text-section" >
 				<span className="sb-bold black-text sb-shop-small-text">{product.brand}</span>
 				<br />
-				<span className="sb-shop-small-text">{product.name}</span>
+					<Link to={`${URL_ROOT}/shop/${product.slug}`}>
+						<span className="sb-shop-small-text black-text">{product.name}</span>
+					</Link>
 				<br />
 				<div className="sb-shop-price-box">
-					<span className="sb-shop-small-text sb-shop-price-text">$ {product.price}</span>
+					<span className="sb-shop-small-text sb-bold sb-shop-price-text">$ {product.price}</span>
 				</div>
 			</div>
 	</div>
@@ -93,8 +80,8 @@ const ShopList = ({
 	handlePage,
 	activePage
 }) => (
-	<div className="col l10 s12">
-		<div className="card white">
+	<div className="col l10 s12 card white">
+		<div className="sb-shop-list">
       <div className="row">
         {children}
       </div>
