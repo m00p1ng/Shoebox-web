cb372f7b48aa51928c3d480423c3c08720215dfa
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-23 19:29:05 +0700

    delete commented code in searh

--- src/components/Search/SearchItem.js
+++ src/components/Search/SearchItem.js
@@ -3,27 +3,6 @@ import { Link } from 'react-router'
 import { URL_ROOT } from 'endpoint'
 
 const SearchItem = ({product}) => (
-  // <div className="col l3 m4 s6 sb-shop-item caard">
-	// 	<div>
-		// <Link to={`${URL_ROOT}/shop/${product.slug}`}>
-			// <img
-			// 	src={product.picture}
-			// 	alt={product.slug} />
-		// </Link>
-	// 	</div>
-	// 	<div className="sb-shop-text-section" >
-	// 		<span className="sb-bold black-text sb-shop-small-text">{product.brand}</span>
-	// 		<br />
-	// 			<Link to={`${URL_ROOT}/shop/${product.slug}`}>
-	// 				<span className="sb-shop-small-text black-text">{product.name}</span>
-	// 			</Link>
-	// 		<br />
-	// 		<div className="sb-shop-price-box">
-	// 			<span className="sb-shop-small-text sb-bold sb-shop-price-text">$ {product.price}</span>
-	// 		</div>
-	// 	</div>
-	// </div>
-
   <div className="col l3 m4 s6 sb-shop-card-item center">
     <div className="card white" >
     <div>
