ed2718533375773ce78978b56f5aaf851fc1da33
moopings <mongkonchai4412@gmail.com>
2016-11-26 18:11:06 +0700

    add manage product pagination

--- src/containers/Manage/Product/ManProductApp.js
+++ src/containers/Manage/Product/ManProductApp.js
@@ -5,19 +5,32 @@ import { loadProducts } from '../../../actions/manage/manProduct'
 
 class ManProductAppContainer extends Component {
   componentDidMount() {
-    this.props.loadProducts()
+    this.props.loadProducts(20, 1)
+  }
+
+  handlePage(page) {
+    this.props.loadProducts(20, page)
   }
 
   render() {
     return (
       <ManProductApp
-        products={this.props.products}/>
+        products={this.props.products}
+        error={this.props.error}
+        totalPage={this.props.totalPage}
+        totalProduct={this.props.totalProduct}
+        activePage={this.props.page}
+        handlePage={this.handlePage.bind(this)} />
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  products: state.manProduct.products
+  products: state.manProduct.products,
+  totalPage: state.manProduct['totalPage'],
+  error: state.manProduct['error'],
+  totalProduct: state.manProduct['totalProduct'],
+  page: state.manProduct['page']
 })
 
 const mapDispatchToProps = ({
