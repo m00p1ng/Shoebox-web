04fcfc4dc79925d1a2b12a6c1f8fcb6026b6f5b5
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-28 09:23:51 +0700

    add /shop page

--- src/components/Shop/ShopApp.js
+++ src/components/Shop/ShopApp.js
@@ -13,8 +13,34 @@ const ShopApp = (props) => (
         <ShopMenu />
 
         <div className="col l10 card">
-          <div className="row">
-            eiei
+          <div className="row center">
+            <div className="sb-shop-header-bar sb-color-pink">
+              Shoebox
+            </div>
+          </div>
+          <div className="row sb-shop-category-row">
+            <div className="col l6">
+              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/O70G235.png" alt="" />
+            </div>
+            <div className="col l6">
+              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/7tMVows.png" alt="" />
+            </div>
+          </div>
+          <div className="row sb-shop-category-row">
+            <div className="col l6">
+              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/GKx2dBu.png" alt="" />
+            </div>
+            <div className="col l6">
+              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/DwTcv7E.png" alt="" />
+            </div>
+          </div>
+          <div className="row sb-shop-category-row">
+            <div className="col l6">
+              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/6pyHZQ1.png" alt="" />
+            </div>
+            <div className="col l6">
+              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/JjhmvUw.png" alt="" />
+            </div>
           </div>
         </div>
 
