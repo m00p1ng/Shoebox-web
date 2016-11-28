781b7dc4794010807495ecb7a961ecd9b756611b
watcharaphat <watcharaphat.3166@gmail.com>
2016-11-28 11:36:32 +0700

    add link to shop category

--- src/components/Shop/ShopApp.js
+++ src/components/Shop/ShopApp.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { HeaderBarApp } from '../App/HeaderBarApp'
+import { Link } from 'react-router'
+import { URL_ROOT } from 'endpoint'
 import ShopMenu from './ShopMenu'
 import renderShopList from './ShopList'
 
@@ -20,26 +22,38 @@ const ShopApp = (props) => (
           </div>
           <div className="row sb-shop-category-row">
             <div className="col l6">
-              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/O70G235.png" alt="" />
+            <Link to={`${URL_ROOT}/shop/all`}>
+              <img className="responsive-img" src="http://i.imgur.com/O70G235.png" alt="" />
+            </Link>
             </div>
             <div className="col l6">
-              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/7tMVows.png" alt="" />
+            <Link to={`${URL_ROOT}/shop/feature`}>
+              <img className="responsive-img" src="http://i.imgur.com/7tMVows.png" alt="" />
+            </Link>
             </div>
           </div>
           <div className="row sb-shop-category-row">
             <div className="col l6">
-              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/GKx2dBu.png" alt="" />
+            <Link to={`${URL_ROOT}/shop/new-arrival`}>
+              <img className="responsive-img" src="http://i.imgur.com/GKx2dBu.png" alt="" />
+            </Link>
             </div>
             <div className="col l6">
-              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/DwTcv7E.png" alt="" />
+            <Link to={`${URL_ROOT}/shop/best-seller`}>
+              <img className="responsive-img" src="http://i.imgur.com/DwTcv7E.png" alt="" />
+            </Link>
             </div>
           </div>
           <div className="row sb-shop-category-row sb-shop-category-last-row">
             <div className="col l6">
-              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/6pyHZQ1.png" alt="" />
+            <Link to={`${URL_ROOT}/shop/most-views`}>
+              <img className="responsive-img" src="http://i.imgur.com/6pyHZQ1.png" alt="" />
+            </Link>
             </div>
             <div className="col l6">
-              <img max-height="180px" className="responsive-img" src="http://i.imgur.com/JjhmvUw.png" alt="" />
+            <Link to={`${URL_ROOT}/shop/brand/nike`}>
+              <img className="responsive-img" src="http://i.imgur.com/JjhmvUw.png" alt="" />
+            </Link>
             </div>
           </div>
         </div>
